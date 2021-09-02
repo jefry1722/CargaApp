4797fc946e648b6328cdd4e04ff15129c20ed5e3
oscarlozano28 <oscar-lozano1@upc.edu.co>
2021-09-02 09:36:55 -0500

    Interfaz de inicio y registro

--- app/src/main/java/co/edu/unipiloto/cargaapp/Login.java
+++ app/src/main/java/co/edu/unipiloto/cargaapp/Login.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
@@ -19,5 +21,10 @@ public class Login extends AppCompatActivity {
         String nombreText = intent.getStringExtra(NOMBRE_USER);
         TextView nombreView = findViewById(R.id.nombre);
         nombreView.setText(nombreText);
+    }
+
+    public void onCerrarSesion (View view){
+        Intent i = new Intent (this, MainActivity.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/co/edu/unipiloto/cargaapp/MainActivity.java
+++ app/src/main/java/co/edu/unipiloto/cargaapp/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent i = getIntent();
     }
 
     public void onButton(View view){
